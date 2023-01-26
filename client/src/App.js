4e4309056f0c4303bf39d3f212925eaba481c4cd
Jan-Kiezun <j4n3k777@gmail.com>
2023-01-26 09:51:07 +0100

    client init

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <img
+        width="64"
+        alt="Twitter-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/64px-Twitter-logo.svg.png"
+      />
+    </div>
+  );
+}
+
+export default App;
